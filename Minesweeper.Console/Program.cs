ae61b9049e57d8543220b40f6ece15fd3f263ed3
Miles Pool <Miles.pool@7digital.com>
2014-10-30 13:21:47 +0000

    Added console project

---
+++ Minesweeper.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Minesweeper.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("|===============================================|");
+            System.Console.WriteLine("||  ____    ____   ______   ___  ___   _____   ||");
+            System.Console.WriteLine("|| |    \\  /    | |_    _| |   \\|   | |   __|  ||");
+            System.Console.WriteLine("|| |     \\/     |   |  |   |        | |  |_    ||");
+            System.Console.WriteLine("|| |            |   |  |   |        | |   _|   ||");
+            System.Console.WriteLine("|| |    |\\/|    |  _|  |_  |        | |  |__   ||");
+            System.Console.WriteLine("|| |____|  |____| |______| |___|\\___| |_____|  ||");
+            System.Console.WriteLine("||                                             ||");
+            System.Console.WriteLine("||          -- S  W  E  E  P  E  R --          ||");
+            System.Console.WriteLine("|===============================================|");
+
+            System.Console.ReadKey();
+        }
+    }
+}
